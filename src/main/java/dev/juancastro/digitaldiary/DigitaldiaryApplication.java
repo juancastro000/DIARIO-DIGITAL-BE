25b546f60dd6c6fec19949217c6b40f41335faeb
juancastro000 <cdyutub@gmail.com>
2025-03-17 21:21:00 +0100

    First commit

---
+++ src/main/java/dev/juancastro/digitaldiary/DigitaldiaryApplication.java
@@ -0,0 +1,13 @@
+package dev.juancastro.digitaldiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitaldiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitaldiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/juancastro/digitaldiary/DigitaldiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.juancastro.digitaldiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitaldiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
